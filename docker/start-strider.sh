129df53fa30880bec703bb52776fe7f4e004daaa
Ryan Taylor Long <ryan@rtlong.com>
2014-06-15 21:51:32 -0700

    Try removing extra env vars in docker/start-strider.sh

--- docker/start-strider.sh
+++ docker/start-strider.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-if [ -n "$DB_URI" ]; then
-  MONGODB_URI=${DB_URI}
-elif [ -z "$MONGODB_URI" ]; then
-  MONGODB_URI=mongodb://localhost/strider-foss
+if [ -z "$DB_URI" ]; then
+  DB_URI=mongodb://localhost/strider-foss
 fi
+
 if [ -n "$MONGO_PORT" ]; then
-  MONGODB_URI="mongodb://${MONGO_PORT#tcp://}/strider-foss"
+  DB_URI="mongodb://${MONGO_PORT#tcp://}/strider-foss"
 fi
-export DB_URI=$MONGODB_URI
+
+# export DB_URI=$MONGODB_URI
 # TODO: make this work with arbitrary plugin_* vars
-export SERVER_NAME=$SERVER_NAME 
-export SMTP_HOST=$SMTP_HOST 
-export SMTP_USER=$SMTP_USER 
-export SMTP_PASS=$SMTP_PASS 
-export SMTP_FROM=$SMTP_FROM 
-export PLUGIN_GITHUB_APP_ID=$PLUGIN_GITHUB_APP_ID 
-export PLUGIN_GITHUB_SECRET=$PLUGIN_GITHUB_SECRET 
-export NODE_ENV=production 
+
+# export SERVER_NAME=$SERVER_NAME
+# export SMTP_HOST=$SMTP_HOST
+# export SMTP_USER=$SMTP_USER
+# export SMTP_PASS=$SMTP_PASS
+# export SMTP_FROM=$SMTP_FROM
+# export PLUGIN_GITHUB_APP_ID=$PLUGIN_GITHUB_APP_ID
+# export PLUGIN_GITHUB_APP_SECRET=$PLUGIN_GITHUB_APP_SECRET
+
+export NODE_ENV=production
 env
 
 exec bin/strider $@
